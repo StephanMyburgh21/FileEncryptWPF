1aa7aec7e86b9099b10360a505f7ed8dd142a69d
stephanmyburgh21@gmail.com <stephanmyburgh21@gmail.com>
2025-06-08 22:16:44 +0200

    Cleaned Startup

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,30 +9,34 @@ namespace FileEncryptWPF
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-            string[] args = e.Args;
+            var args = e.Args;
 
-            if (args is null || args.Length < 2 || (args[1] != "e" && args[1] != "d"))
+            if (args is not [_, "e" or "d"])
             {
                 Process.GetCurrentProcess().Kill();
                 return;
             }
 
-            if (Path.GetExtension(args[0]) == ".encr" && args[1] == "e")
+            string filePath = args[0];
+            string mode = args[1];
+            string ext = Path.GetExtension(filePath);
+
+            if (ext == ".encr" && mode == "e")
             {
                 MessageBox.Show("File is already encrypted", "Cannot Encrypt", MessageBoxButton.OK, MessageBoxImage.Information);
                 Process.GetCurrentProcess().Kill();
                 return;
             }
 
-            if (Path.GetExtension(args[0]) != ".encr" && args[1] == "d")
+            if (ext != ".encr" && mode == "d")
             {
                 MessageBox.Show("File is not encrypted", "Cannot Decrypt", MessageBoxButton.OK, MessageBoxImage.Information);
                 Process.GetCurrentProcess().Kill();
                 return;
             }
 
-            PasswordWindow p = new(args[1][0], args[0]);
-            p.Show(); 
+            new PasswordWindow(mode[0], filePath).Show();
+
         }
     }
 
